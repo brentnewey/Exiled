29bfa5ea3f594f1891e8b1037ea9052f9bf21c95
Brent <brent.newey@gmail.com>
2023-02-12 12:25:28 -0500

    Generate random rocks.

--- Assets/Scripts/TilemapInitializer.cs
+++ Assets/Scripts/TilemapInitializer.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = System.Random;
 
 public class TilemapInitializer : MonoBehaviour
 {
@@ -10,7 +12,17 @@ public class TilemapInitializer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        map.SetTile(new Vector3Int(1, 1, 0), tile);
+        var rand = new Random();
+        for (int i = -10; i <= 10; i++)
+        {
+            for(int j = -10; j <= 10; j++)
+            {
+                if(rand.Next(1, 11) == 1)
+                {
+                    map.SetTile(new Vector3Int(i, j, 0), tile);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
