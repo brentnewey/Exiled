2a417d0de25cfc80b664aa77ca76dce8cb9a28eb
Brent <brent.newey@gmail.com>
2023-02-12 12:12:20 -0500

    Place a rock programatically.

--- Assets/Scripts/TilemapInitializer.cs
+++ Assets/Scripts/TilemapInitializer.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TilemapInitializer : MonoBehaviour
 {
+    public Tilemap map;
+    public Tile tile;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start");
+        map.SetTile(new Vector3Int(1, 1, 0), tile);
     }
 
     // Update is called once per frame
